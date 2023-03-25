4c41631f3ee1e379e7cac0ef89b68e69b9233ffc
arkelley77 <arkelley77@gmail.com>
2023-03-25 15:30:56 -0500

    update includes

--- include/bit-fiddling.hpp
+++ include/bit-fiddling.hpp
@@ -2,10 +2,10 @@
 
 // bit-fiddling.hpp
 namespace kel {
-# define pow2(nbits, power) (static_cast<u##nbits##>(1) << (power))
-# define ones(nbits, num) (pow2(nbits, num) - static_cast<u##nbits##>(1))
-# define bitRange(nbits, start, stop) (ones(nbits, stop) & (~ones(nbits, start)))
+#define pow2(power) (1ull << (power))
+#define ones(num) (pow2(num) - 1ull)
+#define bitRange(nbits, start, stop) (ones(nbits, stop) & (~ones(nbits, start)))
 
-# define getLS1B(mask) ((mask) & -(mask))
-# define clearLS1B(mask) (mask &= (mask - static_cast<decltype(mask)>(1)))
+#define getLS1B(mask) ((mask) & -(mask))
+#define clearLS1B(mask) (mask &= (mask - static_cast<decltype(mask)>(1)))
 }
--- include/lookup-tables.hpp
+++ include/lookup-tables.hpp
@@ -7,12 +7,13 @@
 
 // lookup-tables.hpp
 namespace kel {
-# define rtype(fn) decltype(fn::eval(std::declval<size_t>()))
-# define rtype2d(fn) decltype(fn::eval(std::declval<size_t>(), std::declval<size_t>()))
+#define rtype(fn) decltype(fn::eval(std::declval<size_t>()))
+#define rtype2d(fn) decltype(fn::eval(std::declval<size_t>(), std::declval<size_t>()))
 
   template <class GenFn, size_t N, size_t... Indexes>
-  static constexpr std::array<rtype(GenFn), N> _makeLookupTable(std::index_sequence<Indexes...>) noexcept {
-    constexpr std::array<rtype(GenFn), N> table{ { GenFn::eval(Indexes)... } };
+  static constexpr std::array<rtype(GenFn), N> 
+    _makeLookupTable(std::index_sequence<Indexes...>) noexcept {
+    constexpr std::array<rtype(GenFn), N> table{{GenFn::eval(Indexes)...}};
     return table;
   }
   template <class GenFn, size_t N, size_t... Indexes>
@@ -20,24 +21,34 @@ namespace kel {
     return _makeLookupTable<GenFn, N>(std::make_index_sequence<N>{});
   }
 
-# define genLookupTable(gen_fn, size)                                                         \
-struct gen_##gen_fn {                                                                         \
-  static constexpr auto eval(size_t input) noexcept { return gen_fn(input); }                 \
-};                                                                                            \
-constexpr auto lut_##gen_fn = kel::makeLookupTable<gen_##gen_fn, size>()
-# define lookup(gen_fn, index) (lut_##gen_fn[index])
+#define genLookupTable(gen_fn, size)                                           \
+  struct gen_##gen_fn                                                          \
+  {                                                                            \
+    static constexpr auto eval(size_t input) noexcept                          \
+    {                                                                          \
+      return gen_fn(input);                                                    \
+    }                                                                          \
+  };                                                                           \
+  constexpr auto lut_##gen_fn = kel::makeLookupTable<gen_##gen_fn, size>()
+#define lookup(gen_fn, index) (lut_##gen_fn[index])
 
-# define genLookupTable2d(gen_fn, width, height)                                              \
-struct gen_##gen_fn {                                                                         \
-  static constexpr size_t w = width;                                                          \
-  static constexpr auto eval(size_t input) noexcept { return gen_fn(input % w, input / w); }  \
-};                                                                                            \
-constexpr auto lut_##gen_fn = kel::makeLookupTable<gen_##gen_fn, width * height>()
-# define lookup2d(gen_fn, x, y) (lut_##gen_fn[x + gen_##gen_fn::w * y])
+#define genLookupTable2d(gen_fn, width, height)                                \
+  struct gen_##gen_fn                                                          \
+  {                                                                            \
+    static constexpr size_t w = width;                                         \
+    static constexpr auto eval(size_t input) noexcept                          \
+    {                                                                          \
+      return gen_fn(input % w, input / w);                                     \
+    }                                                                          \
+  };                                                                           \
+  constexpr auto lut_##gen_fn =                                                \
+    kel::makeLookupTable<gen_##gen_fn, width * height>()
+#define lookup2d(gen_fn, x, y) (lut_##gen_fn[x + gen_##gen_fn::w * y])
 
-# undef rtype
-# undef rtype2d
+#undef rtype
+#undef rtype2d
 }
+
 
 // Example 1:
 static constexpr int popcnt(size_t mask) noexcept {
